683f999c7521c5ef0afbbc610608ea3407dfdde6
Omar Garcia <omar_yo2003@hotmail.com>
2024-11-05 20:42:12 -0700

    se agrego seeder para base de datos

---
+++ prisma/products.ts
@@ -0,0 +1,14 @@
+export const products=[
+    {
+        name: "Laptop 2",
+    description: "laptop de 14\"",
+    price: 9999.98,
+    image: "https://m.media-amazon.com/images/I/41PyspVpJ4L._AC_UF894,1000_QL80_.jpg",
+    },
+    {
+        name: "Tv 2",
+    description: "tv de 50 pulgadas",
+    price: 10000.5,
+    image: "https://res.cloudinary.com/sharp-consumer-eu/image/fetch/w_3000,f_auto/https://s3.infra.brandquad.io/accounts-media/SHRP/DAM/origin/de0d3686-4766-11ee-8cfb-8ee861fd9236.jpg",
+    }
+]
