c238aad66b1fa828578b0b0c06efdecd8443265b
Omar Garcia <omar_yo2003@hotmail.com>
2024-11-22 16:41:23 -0700

    test cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: 'https://frontend-nextjs-zeta.vercel.app', // Allow specific origins
-    methods: 'GET,POST,PUT,DELETE,PATCH', // Allow specific HTTP methods
+    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS', // Allow specific HTTP methods
     credentials: true, // Allow credentials (cookies, authorization headers)
     allowedHeaders: 'Content-Type, Authorization', // Allow specific headers
   });
