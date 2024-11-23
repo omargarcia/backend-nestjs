e98410dcda1cc126227dfc55cba9e7decb9cb732
Omar Garcia <omar_yo2003@hotmail.com>
2024-11-23 12:14:43 -0700

    correct origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: 'https://backend-nestjs-psi.vercel.app', // Allow specific origins
+    origin: 'https://frontend-nextjs-zeta.vercel.app', // Allow specific origins
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS', // Allow specific HTTP methods
     credentials: true, // Allow credentials (cookies, authorization headers)
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma', // Allow specific headers
