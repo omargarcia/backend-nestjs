ba6c1934005b242b75defe0aa560d2f3f08cdf07
Omar Garcia <omar_yo2003@hotmail.com>
2024-11-22 17:21:49 -0700

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
-  // app.enableCors({
-  //   origin: '*', // Allow specific origins
-  //   methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS', // Allow specific HTTP methods
-  //   credentials: true, // Allow credentials (cookies, authorization headers)
-  //   allowedHeaders: 'Content-Type, Authorization', // Allow specific headers
-  // });
+  app.enableCors({
+    origin: 'https://backend-nestjs-psi.vercel.app', // Allow specific origins
+    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS', // Allow specific HTTP methods
+    credentials: true, // Allow credentials (cookies, authorization headers)
+    allowedHeaders: 'Content-Type, Authorization', // Allow specific headers
+  });
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
