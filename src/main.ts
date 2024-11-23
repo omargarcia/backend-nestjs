73dc5419ae905167fe0506e5e2ae857250d31ea8
Omar Garcia <omar_yo2003@hotmail.com>
2024-11-23 12:11:34 -0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     origin: 'https://backend-nestjs-psi.vercel.app', // Allow specific origins
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS', // Allow specific HTTP methods
     credentials: true, // Allow credentials (cookies, authorization headers)
-    allowedHeaders: 'Content-Type, Authorization', // Allow specific headers
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma', // Allow specific headers
   });
   await app.listen(process.env.PORT ?? 4000);
 }
